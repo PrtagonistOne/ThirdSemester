0415c7645425855d64cfe421bb62026f3d3c3982
PrtagonistOne <50425598+PrtagonistOne@users.noreply.github.com>
2019-11-06 00:49:48 +0200

    summ

---
+++ LAB8/Problem_1/Problem_1/MathOperations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Problem_1
+{
+    class MathOperations
+    {
+        public int  Add(int a, int b)
+        {
+
+            return a + b;
+        }
+
+        public double Add(double a, double b, double z)
+        {
+
+            return a + b + z;
+        }
+
+        public decimal Add(decimal a, decimal b, decimal z)
+        {
+
+            return a + b + z;
+        }
+    }
+}
--- LAB8/Problem_1/Problem_1/Program.cs
+++ LAB8/Problem_1/Problem_1/Program.cs
@@ -4,9 +4,13 @@ namespace Problem_1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            MathOperations mo = new MathOperations();
+
+            Console.WriteLine($"sum of integer: {mo.Add(2,2)}");
+            Console.WriteLine($"sum of double: {mo.Add(2.2, 5.5, 3.3)}");
+            Console.WriteLine($"sum of decimal: {mo.Add(2.2m, 5.5m, 4.4m)}");
         }
     }
 }

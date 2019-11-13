4ba8619fabbdbe80ac8c72e55011c2aa10a163c4
PrtagonistOne <50425598+PrtagonistOne@users.noreply.github.com>
2019-11-14 01:52:50 +0200

    2

--- LAB9/Problem_1/Problem_1/Program.cs
+++ LAB9/Problem_1/Problem_1/Program.cs
@@ -4,9 +4,20 @@ namespace Problem_1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Amount of strings: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input the strings: ");
+            for (int i=0;i<n;i++)
+            {
+                string t = Console.ReadLine();
+                Box<string> boxy = new Box<string>(t);
+                Console.WriteLine(boxy);
+            }
+
         }
     }
 }

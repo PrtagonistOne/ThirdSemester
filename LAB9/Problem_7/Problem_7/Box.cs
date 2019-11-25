fc9a081675d20ac42c516fdf74339bcc494fbddf
Oleksander <50425598+PrtagonistOne@users.noreply.github.com>
2019-11-26 00:40:12 +0200

    ew

---
+++ LAB9/Problem_7/Problem_7/Box.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace Problem_7
+{
+    class Box<T> where T : IComparable
+    {
+        T[] Boxxy;
+
+        public Box() 
+        { 
+            Boxxy = new T[0];
+        }
+
+        public void Add(T element)
+        {
+             int n = Boxxy.Length + 1;
+
+            Array.Resize(ref Boxxy, n);
+
+            for (int i = n - 1; i < n; i++)
+            {
+                Boxxy[i] = element; 
+            }
+        }
+
+        public void Remove(int indx)
+        {
+            int n = Boxxy.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i == indx)
+                {
+                    for (int j = i; j < n - 1; j++)
+                        Boxxy[j] = Boxxy[j + 1];
+                    n--;
+                    Array.Resize(ref Boxxy, n);
+                }
+            }
+        }
+
+        public bool Contains(T element)
+        {
+            bool IsContains = false;
+
+            for(int i = 0; i < Boxxy.Length;i++)
+            {
+                if(element.CompareTo(Boxxy[i]) == 0)
+                { 
+                    IsContains = true;
+                }
+            }
+
+            return IsContains;
+        }
+
+        public void Swap(int indx1, int indx2)
+        {
+            T t = Boxxy[indx1];
+            Boxxy[indx1] = Boxxy[indx2];
+            Boxxy[indx2] = t;
+        }
+
+        public void Greater(T element)
+        {
+            int c = 0;
+            for(int i=0;i< Boxxy.Length;i++)
+            {
+                if(element.CompareTo(Boxxy[i]) < 0)
+                {
+                    c++;
+                }
+            }
+
+            Console.WriteLine(c);
+        }
+
+        public void Max()
+        {
+            T max = Boxxy[0];
+            for(int i=0;i< Boxxy.Length;i++)
+            {
+                if(max.CompareTo(Boxxy[i]) < 0)
+                {
+                    max = Boxxy[i];
+                }
+            }
+
+            Console.WriteLine(max);
+        }
+
+        public void Min()
+        {
+            T min = Boxxy[0];
+            for (int i = 0; i < Boxxy.Length; i++)
+            {
+                if (min.CompareTo(Boxxy[i]) > 0)
+                { 
+                    min = Boxxy[i]; 
+                }
+            }
+
+            Console.WriteLine(min);
+        }
+
+        public void Display()
+        {
+            for(int i=0;i< Boxxy.Length;i++)
+            {
+                Console.WriteLine(Boxxy[i]);
+            }
+        }
+    }
+}
---
+++ LAB9/Problem_7/Problem_7/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Problem_7
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] start = Console.ReadLine().Split(" ");
+
+            Box<string> boxxy = new Box<string>();
+
+            while (start[0] != "END")
+            {
+                if (start[0] == "Add" && start.Length == 2)
+                {
+                    boxxy.Add(start[1]);
+                }
+
+                else if (start[0] == "Remove" && start.Length == 2)
+                {
+                    boxxy.Remove(int.Parse(start[1]));
+                }
+
+                else if (start[0] == "Contains" && start.Length == 2)
+                {
+                    Console.WriteLine(boxxy.Contains(start[1]));
+                }
+
+                else if (start[0] == "Swap" && start.Length == 3)
+                {
+                    boxxy.Swap(int.Parse(start[1]), int.Parse(start[2]));
+                }
+
+                else if (start[0] == "Greater" && start.Length == 2)
+                {
+                    boxxy.Greater(start[1]);
+                }
+
+                else if (start[0] == "Max" && start.Length == 1)
+                {
+                    boxxy.Max();
+                }
+
+                else if (start[0] == "Min" && start.Length == 1)
+                {
+                    boxxy.Min();
+                }
+
+                else if (start[0] == "Print" && start.Length == 1)
+                {
+                    boxxy.Display();
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid command!");
+                }
+
+                start = Console.ReadLine().Split(" ");
+            }
+        }
+    }
+}

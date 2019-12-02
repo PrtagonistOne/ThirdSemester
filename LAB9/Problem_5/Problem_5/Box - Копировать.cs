6b94ce1f63119b0f166c97ddb92a12d9a1f2757d
Oleksander <50425598+PrtagonistOne@users.noreply.github.com>
2019-12-02 19:52:02 +0200

    1

---
+++ LAB9/Problem_5/Problem_5/Box - Копировать.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Problem_5
+{
+    class Differece<T> 
+
+    {
+       
+        public void Riznitsya( object x, object y )
+        {
+            if (x.GetType() == typeof(int) && y.GetType() == typeof(int))
+            {
+                int c1 = (int)x + (int)y;
+
+                Console.WriteLine(c1);
+            }
+             
+            else if (x.GetType() == typeof(double) && y.GetType() == typeof(double))
+            {
+                double c1 = (double)x + (double)y;
+
+                Console.WriteLine(c1);
+            }
+            else if(x.GetType() == typeof(decimal) && y.GetType() == typeof(decimal))
+            {
+                decimal c1 = (decimal)x + (decimal)y;
+
+                Console.WriteLine(c1);
+            }
+            else if(x.GetType() == typeof(float) && y.GetType() == typeof(float))
+            {
+                decimal c1 = (decimal)x + (decimal)y;
+
+                Console.WriteLine(c1);
+            }
+            else if(x.GetType() == typeof(string) && y.GetType() == typeof(string))
+            {
+
+                Console.WriteLine("Нельзя отнимать строки!");
+            }
+            else
+                Console.WriteLine("Неверное приведение типов!");
+        }
+    }
+}
--- LAB9/Problem_5/Problem_5/Program.cs
+++ LAB9/Problem_5/Problem_5/Program.cs
@@ -4,27 +4,9 @@ namespace Problem_5
 {
     class Program
     {
-        static void Main()
-        {
-            Console.WriteLine("Input the abpunt of strings to compare: ");
-            int n = int.Parse(Console.ReadLine());
+        Differece<int> nik = new Differece<int> { x = 300, y = 100 };
+        
+        
 
-            string s;
-            string[] s1 = new string[n];
-
-            for (int i = 0; i < n; i++)
-            {
-                s = Console.ReadLine();
-                s1[i] = s;
-            }
-
-            Box<string> toCompare = new Box<string>(s1);
-
-            Console.WriteLine("Input the string that is need to be compared with: ");
-            s = Console.ReadLine();
-            string[] th = s.Split();
-
-            Console.WriteLine(toCompare.Compare(th));
-        }
     }
 }

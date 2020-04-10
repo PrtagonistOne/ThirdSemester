d568a4487090e795742c7c22651bad9829b52090
Oleksander <50425598+PrtagonistOne@users.noreply.github.com>
2020-04-10 15:04:23 +0300

    ds

---
+++ LAB14/Problem_6/Problem_6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+namespace Problem_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] chars = { '.', ',', ':', ';', '=', '(', ')', '&', '[', ']', '\"', '\'', '\\', '/', '!', '?', ' ' };
+            string[] sentence = input.Split(chars, StringSplitOptions.RemoveEmptyEntries);
+
+            string[] seperators = input.Split(sentence, StringSplitOptions.RemoveEmptyEntries);
+            int n = sentence.Length;
+            sentence = sentence.Reverse().ToArray();
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(sentence[i] + seperators[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LAB3/Problem_7/Problem_7/SomeClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem_7
+{
+    public class SomeClass
+    {
+        public string SomeProb { get; set; }
+        public SomeClass(string someProb)
+        {
+            SomeProb = someProb;
+        }
+    }
+}
